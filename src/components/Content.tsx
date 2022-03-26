de43ccd3b153728b27e0f9992434e6bb4dc7ba84
Vinay Ayyala <vinay@commandbar.com>
2022-03-26 00:02:54 -0700

    fix(nav): Add auto-scroll to top on nav change

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,15 +2,18 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import CompaniesTable from "./content/CompaniesTable";
 import CompanyDetail from "./content/CompanyDetail";
 import CompanyEdit from "./content/CompanyEdit";
+import ScrollToTop from "./ScrollToTop";
 
 const Content = () => {
   return (
-    <Routes>
-      <Route path="/leads" element={<CompaniesTable />} />
-      <Route path="/leads/:id" element={<CompanyDetail />} />
-      <Route path="/leads/:id/edit" element={<CompanyEdit />} />
-      <Route path="*" element={<Navigate to="/leads" replace />} />
-    </Routes>
+    <ScrollToTop>
+      <Routes>
+        <Route path="/leads" element={<CompaniesTable />} />
+        <Route path="/leads/:id" element={<CompanyDetail />} />
+        <Route path="/leads/:id/edit" element={<CompanyEdit />} />
+        <Route path="*" element={<Navigate to="/leads" replace />} />
+      </Routes>
+    </ScrollToTop>
   );
 };
 export default Content;
