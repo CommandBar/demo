86962378ace5b58fc52a2bbba612e36ce77b11d8
Vinay Ayyala <vinay@commandbar.com>
2022-03-29 17:56:03 -0700

    Remove "Add a reminder"

--- src/components/content/CompanyDetail.tsx
+++ src/components/content/CompanyDetail.tsx
@@ -58,12 +58,6 @@ export default function CompanyDetail() {
             <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
               <button
                 type="button"
-                className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
-              >
-                Add a reminder
-              </button>
-              <button
-                type="button"
                 className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
                 onClick={() => navigate(`/leads/${company.id}/edit`)}
               >
